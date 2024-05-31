8ab0c52ba26be432689682fe11baf644240c1b14
JeferssonTrejos <jeffersonbladimirlovo@gmail.com>
2024-05-31 12:50:57 -0600

    fix port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
-const port = process.env.MONGO_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
