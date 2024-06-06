59ab3899e5fe42e261a7d6b84ce34cfdb46d12a9
JeferssonTrejos <jeffersonbladimirlovo@gmail.com>
2024-06-06 09:11:50 -0600

    update

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const modulos = require('./routers/router');
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.MONGO_PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api/curso', modulos);
+app.use('/', express.static('public'));
+
+const MONGO_DB_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/dbcurso?authSource=admin';
+
+mongoose.connect(MONGO_DB_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión de MongoDB:'));
+db.once('open', () => {
+    console.log('Conectado a MongoDB');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
+
